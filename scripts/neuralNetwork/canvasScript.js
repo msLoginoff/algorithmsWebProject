afbd1928f1ef47fddcc7d331ccb6ef201709f8e0
msLoginoff <146209985+msLoginoff@users.noreply.github.com>
2024-04-02 21:16:39 +0700

    add canvasScript.js and background

---
+++ scripts/neuralNetwork/canvasScript.js
@@ -0,0 +1,98 @@
+function DCanvas(el) {
+    let canvasVariable = document.getElementById('canv');
+    const ctx = el.getContext('2d');
+    const pixels = 10;
+
+    let IsMouseDown = false;
+
+    canvasVariable.width = 50;
+    canvasVariable.height = 50;
+    this.clear = function () {
+        ctx.clearRect(0, 0, canvasVariable.width, canvasVariable.height);
+    }
+
+    this.calculate = function () {
+        ctx.scale(0.056, 0.056);
+        const w = canvasVariable.width;
+
+        const h = canvasVariable.height;
+        const p = 28;
+
+        const xStep = w / p;
+        const yStep = h / p;
+
+        const vector = [];
+        let data = [];
+        let counter = 0;
+
+        for ( let y = 0; y < h; y += yStep) {
+            for (let x = 0; x < w; x += xStep) {
+                data[counter] = ctx.getImageData(x, y, xStep, yStep);
+                vector.push(data[counter++].data[3] / 255)
+            }
+        }
+
+        for (let i = 0; i < p + 1; i++) {
+            vector.pop()
+        }
+
+        let tmp = 0;
+        for (let i = 0; i < p; i++) {
+            vector.splice((p + 1) * i + p - tmp, 1);
+            tmp++;
+        }
+
+        ctx.scale(17.857142857142858, 17.857142857142858);
+        return vector;
+    }
+
+    el.addEventListener('mousedown', function () {
+        IsMouseDown = true;
+        ctx.beginPath();
+    })
+
+    el.addEventListener('mouseup', function () {
+        IsMouseDown = false;
+    })
+
+    el.addEventListener('mousemove', function (e) {
+        if (IsMouseDown) {
+            ctx.fillStyle = '#ff8800';
+            ctx.strokeStyle = '#ff8800';
+            ctx.lineWidth = 2//pixels + 20;
+
+            ctx.lineTo(e.offsetX, e.offsetY);
+            ctx.stroke();
+
+            ctx.beginPath();
+            ctx.arc(e.offsetX, e.offsetY, 2 / 2, 0, Math.PI * 2);
+            ctx.fill();
+
+            ctx.beginPath();
+            ctx.moveTo(e.offsetX, e.offsetY);
+        }
+    })
+
+}
+
+
+const currentCanvas = new DCanvas(document.getElementById('canv'));
+
+let vectorTest;
+document.addEventListener('keypress', function (e) {
+    if (e.key.toLowerCase() === 'c') {
+        currentCanvas.clear();
+    }
+
+    if (e.key.toLowerCase() === 'v') {
+        vectorTest = currentCanvas.calculate();
+    }
+
+    if (e.key.toLowerCase() === 's') {
+        getNumberResult();
+    }
+})
+
+
+
+
