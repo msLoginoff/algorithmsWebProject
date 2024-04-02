1c856d001e63b8ea6511cf5354c906355db01397
msLoginoff <146209985+msLoginoff@users.noreply.github.com>
2024-04-02 21:33:59 +0700

    rebase to add data files

---
+++ scripts/neuralNetwork/1.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:749aa42b74d4093b7f87ab0a59943bd2dc0b1138cbbc8aee293c4193773dc489
+size 132659595
---
+++ scripts/neuralNetwork/2.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:46da7ee34d928e31db06431fd6f95bb72e9e136619e908841073c07c9bc473a8
+size 5171666
