ed12a5e8ad29bf66748e4d0bca7198806c40b704
Nikita Lugachev <123080758+nu1ts@users.noreply.github.com>
2024-04-19 23:50:05 +0700

    unnecessary folders removed

--- scripts/mainScript.js
+++
@@ -1,4 +0,0 @@
-function windowToCanvas(canvas, x, y) {
-    let bbox = canvas.getBoundingClientRect();
-    return { x: x - bbox.left * (canvas.width / bbox.width), y: y - bbox.top * (canvas.height / bbox.height) };
-}
