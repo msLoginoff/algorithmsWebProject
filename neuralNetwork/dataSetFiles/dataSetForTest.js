c581a2b5f2d3afc60180fbcdd92b4a358b0d55db
msLoginoff <146209985+msLoginoff@users.noreply.github.com>
2024-04-20 08:52:10 +0700

    delete dataset

--- neuralNetwork/dataSetFiles/dataSetForTest.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:15b0feb50176bf213b72c33d84d135e3a1c21f004f887cf6739a91c445fd9765
-size 21562661
--- neuralNetwork/dataSetFiles/dataSetString.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:749aa42b74d4093b7f87ab0a59943bd2dc0b1138cbbc8aee293c4193773dc489
-size 132659595
